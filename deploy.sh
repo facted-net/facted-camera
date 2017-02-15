07e53711012afd0d520c08f2d441a6f142bd13bb
Chuck LeDuc Díaz <celeduc@gmail.com>
2017-02-15 23:33:15 +0100

    deploy the debug version because it works

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p facted_camera_deploy
-cp app/build/outputs/apk/app-release-unsigned.apk facted_camera_deploy/facted_camera.apk
+cp app/build/outputs/apk/app-debug.apk facted_camera_deploy/facted_camera.apk
 cp README.md facted_camera_deploy/README
 cp gpl-3.0.txt facted_camera_deploy/LICENSE
 OUTPUT=$(ipfs add -q -r facted_camera_deploy | tail -1)
