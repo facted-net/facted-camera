d53c52581164f78373f42779857b605ae8110ca0
Chuck LeDuc Díaz <celeduc@gmail.com>
2017-02-27 00:37:20 +0100

    add release notes

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+# 1.0
+Initial version
+
+# 1.1
+Improved camera performance by parallelizing submission of pictures
+and reducing frequency of web service polling for Bitcoin and Ethereum.
--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 mkdir -p facted_camera_deploy
 cp app/build/outputs/apk/app-debug.apk facted_camera_deploy/facted_camera.apk
 cp README.md facted_camera_deploy/README
+cp RELEASE_NOTES.md facted_camera_deploy/RELEASE_NOTES
 cp gpl-3.0.txt facted_camera_deploy/LICENSE
 OUTPUT=$(ipfs add -q -r facted_camera_deploy | tail -1)
 ipfs pin add $OUTPUT
